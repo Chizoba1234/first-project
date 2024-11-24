ca4fbfc7d8d847de294344ca4493ed549f00adcd
Chizoba1234 <christancia34@gmail.com>
2024-11-24 22:02:51 +0800

    Project.shcreator

---
+++ #Projectcreator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Greet user Jessy
+s@DESKTOP-4UHQR8E MINGW64 ~/Desktop/Software develpment/first-project (main)
+$ echo " Good day, jessy!"
+ Good day, jessy!
+
+s@DESKTOP-4UHQR8E MINGW64 ~/Desktop/Software develpment/first-project (main)
+$ echo " Enter your project name:"
+ Enter your project name:
+$ echo projectcreator
+projectcreator
